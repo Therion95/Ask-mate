6036e129a534c5696518c282e07e4e3466099f2b
Maciej M <matysiewskimaciej@gmail.com>
2021-08-14 12:48:17 +0200

    Implemented route for list_questions

--- server.py
+++ server.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+
+@app.route('/list', methods=['get'])
+def list_questions():
+    data, headers = data_manager.list_questions('data/questions.csv')
+    return render_template('list.html', data=data, headers=headers)
+
+
+@app.route('/question/<int:question_id>')
+def question_display(question_id):
+    pass
 
 
 if __name__ == "__main__":
