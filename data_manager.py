fdc7b1abbfbcccfc664256f4c30863fdec00ad04
Maciej M <matysiewskimaciej@gmail.com>
2021-08-15 15:41:15 +0200

    Implemented question_display function

--- data_manager.py
+++ data_manager.py
@@ -6,8 +6,13 @@ def list_questions(csv_file):
 
     headers = list(list_of_data[0].keys())
 
+    # TODO: implement order_by().all() functionality
+
     return list_of_data, headers
 
 
-def question_display():
-    pass
+def question_display(question_id, csv_file):
+    questions, headers = list_questions(csv_file)
+    for question in questions:
+        if int(question['id']) == question_id:
+            return question, headers
