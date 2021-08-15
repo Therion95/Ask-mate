a672430bb57e5729c263dfa6c15dc57fee2ddea2
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2021-08-15 15:45:36 +0200

    requirements.txt changes and file track changed

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import data_manager
 app = Flask(__name__)
 
 
+FILE_NAME = "C:\\Kurs_Programowania\\modul_web_codecool\\week1\\ask-mate-1-python-matysiewsky\\data\\questions.csv"
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -12,7 +15,7 @@ def index():
 
 @app.route('/list', methods=['get'])
 def list_questions():
-    data, headers = data_manager.list_questions('data/questions.csv')
+    data, headers = data_manager.list_questions(FILE_NAME)
     return render_template('list.html', data=data, headers=headers)
 
 
