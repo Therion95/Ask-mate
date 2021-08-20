51a49664934bafff6233d63a48a09a99d9c1b54a
Maciej M <matysiewskimaciej@gmail.com>
2021-08-20 20:09:48 +0200

    Implemenmted question  edit, answer delete, voting for questions and answers

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-import data_manager, connection
-
-
+import connection
+import data_manager
 
 app = Flask(__name__)
 
@@ -18,19 +17,83 @@ def index():
 
 @app.route('/list', methods=['get'])
 def list_questions():
-    global QUESTIONS
+    global QUESTIONS, ANSWERS
     data, headers = data_manager.list_questions(QUESTIONS)
+
     return render_template('list.html', data=data, headers=headers)
 
 
 @app.route('/question/<int:question_id>', methods=['get'])
 def question_display(question_id):
-    global QUESTIONS
+    global QUESTIONS, ANSWERS
     question_to_display, headers, answers = data_manager.question_display(question_id, QUESTIONS, ANSWERS)
+
     return render_template('question.html', question=question_to_display, headers=headers, answers=answers)
 
 
-# TODO: get_or_404()
+@app.route('/question/<int:question_id>/edit', methods=['GET', 'POST'])
+def question_edit(question_id):
+    global QUESTIONS, ANSWERS
+    if request.method == 'GET':
+        question_to_edit = data_manager.question_display(question_id, QUESTIONS, ANSWERS)[0]
+
+        return render_template('question_edit.html', question=question_to_edit)
+
+    elif request.method == 'POST':
+        edited_question = dict(request.form)
+        keys, values = list(edited_question.keys()), list(edited_question.values())
+        connection.csv_editing(QUESTIONS, question_id, keys=keys, values_to_update=values)
+
+        return redirect(url_for('question_display', question_id=question_id))
+
+
+@app.route('/question/<int:question_id>/delete', methods=['GET'])
+def question_delete(question_id):
+    global QUESTIONS
+    connection.csv_delete_row(QUESTIONS, question_id)
+
+    return redirect(url_for('list_questions'))
+
+
+@app.route('/answer/<int:answer_id>/delete', methods=['GET'])
+def answer_delete(answer_id):
+    global ANSWERS
+    question_id = connection.csv_delete_row(ANSWERS, answer_id)
+
+    return redirect(url_for('question_display', question_id=question_id))
+
+
+@app.route('/question/<int:question_id>/vote_up', methods=['GET'])
+def question_voting_up(question_id):
+    global QUESTIONS
+    connection.csv_editing(QUESTIONS, question_id, method='add')
+
+    return redirect(url_for('question_display', question_id=question_id))
+
+
+@app.route('/question/<int:question_id>/vote_down', methods=['GET'])
+def question_voting_down(question_id):
+    global QUESTIONS
+    connection.csv_editing(QUESTIONS, question_id, method='subtract')
+
+    return redirect(url_for('question_display', question_id=question_id))
+
+
+@app.route('/answer/<int:answer_id>/vote_up', methods=['GET'])
+def answer_voting_up(answer_id):
+    global ANSWERS
+    question_id = connection.csv_editing(ANSWERS, answer_id, method='add')
+
+    return redirect(url_for('question_display', question_id=question_id))
+
+
+@app.route('/answer/<int:answer_id>/vote_down', methods=['GET'])
+def answer_voting_down(answer_id):
+    global ANSWERS
+    question_id = connection.csv_editing(ANSWERS, answer_id, method='subtract')
+
+    return redirect(url_for('question_display', question_id=question_id))
+
 
 @app.route('/answer', methods=['GET'])
 def display_answer_question():
