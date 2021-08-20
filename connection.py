4f4dbe7ff6b53d54c80b99fbabf144f1e90a7255
Maciej M <matysiewskimaciej@gmail.com>
2021-08-20 20:07:56 +0200

    Implemented deleting question and voting

--- connection.py
+++ connection.py
@@ -1,6 +1,13 @@
 import csv
 
 
+def get_headers(csv_file):
+    with open(csv_file, "r") as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+    return headers
+
+
 def csv_opening(csv_file):
     # simple function to operate on .csv file - generating dictionary for each line with keys corresponding with
     # column name. function returns a list of dictionaries.
@@ -22,11 +29,57 @@ def csv_creating(csv_file, dict_to_add=None):
 
 
 def csv_appending(csv_file, dict_to_add):
-    with open(csv_file, "r") as file:
-        reader = csv.reader(file)
-        i = next(reader)
-
     with open(csv_file, 'a', newline='') as file:
-        dict_appender = csv.DictWriter(file, fieldnames=i)
+        dict_appender = csv.DictWriter(file, fieldnames=get_headers(csv_file))
         dict_appender.writerow(dict_to_add)
 
+
+def csv_editing(csv_file, given_id, keys=None, values_to_update=None, method=None):
+    with open(csv_file, 'r') as file:
+        headers = get_headers(csv_file)
+        data = csv.DictReader(file)
+        temp_data = []
+        for row in data:
+            if int(row['id']) == given_id:
+                if method == 'add':
+                    row['vote_number'] = str(int(row['vote_number']) + 1)
+                    if 'question_id' in row.keys():
+                        question_id_return = row['question_id']
+                elif method == 'subtract':
+                    row['vote_number'] = str(int(row['vote_number']) - 1)
+                    if 'question_id' in row.keys():
+                        question_id_return = row['question_id']
+                else:
+                    for key, value in zip(keys, values_to_update):
+                        row[key] = value
+                temp_data.append(row)
+            else:
+                temp_data.append(row)
+
+    with open(csv_file, 'w') as file:
+        dict_writer = csv.DictWriter(file, delimiter=',', fieldnames=headers)
+        dict_writer.writeheader()
+        dict_writer.writerows(temp_data)
+
+    if question_id_return:
+        return question_id_return
+
+
+def csv_delete_row(csv_file, question_id):
+    with open(csv_file, 'r') as file:
+        headers = get_headers(csv_file)
+        data = csv.DictReader(file)
+        temp_data = []
+        for row in data:
+            if int(row['id']) != question_id:
+                temp_data.append(row)
+            else:
+                if 'question_id' in row.keys():
+                    question_id_return = row['question_id']
+
+    with open(csv_file, 'w') as file:
+        dict_writer = csv.DictWriter(file, delimiter=',', fieldnames=headers)
+        dict_writer.writeheader()
+        dict_writer.writerows(temp_data)
+
+    return question_id_return
