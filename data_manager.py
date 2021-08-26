b0017fbbfbd1f3fa5c5864f0d983169092a74b3c
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2021-08-26 12:11:57 +0200

    current_date function implementent and added to ans and quest

--- data_manager.py
+++ data_manager.py
@@ -37,8 +37,8 @@ def question_display(question_id, questions_csv_file, answers_csv_file):
 
 def add_question(requested_data, requested_image):
     path = connection.upload_file(requested_image)
-    keys = ['id', 'title', 'message', 'view_number', 'vote_number', 'image']
-    values = [util.get_next_id(QUESTIONS), requested_data['title'], requested_data['message'], 0, 0, path]
+    keys = ['id', 'submission_time', 'title', 'message', 'view_number', 'vote_number', 'image']
+    values = [util.get_next_id(QUESTIONS), util.current_date(), requested_data['title'], requested_data['message'], 0, 0, path]
     prepared_dict = {k:v for k, v in zip(keys, values)}
 
     return connection.csv_appending(QUESTIONS, prepared_dict)
@@ -46,8 +46,8 @@ def add_question(requested_data, requested_image):
 
 def answer_question(requested_data, requested_image, question_id):
     path = connection.upload_file(requested_image)
-    keys = ['id', 'message', 'vote_number', 'question_id', 'image']
-    values = [util.get_next_id(ANSWERS), requested_data['message'], 0, question_id, path]
+    keys = ['id', 'submission_time', 'message', 'vote_number', 'question_id', 'image']
+    values = [util.get_next_id(ANSWERS), util.current_date(), requested_data['message'], 0, question_id, path]
     prepared_dict = {k:v for k, v in zip(keys, values)}
 
     return connection.csv_appending(ANSWERS, prepared_dict)
